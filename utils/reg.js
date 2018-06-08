8424cf62dd5ea6b2e50078f578fce5523d7aebcc
华鹏 <hexris@outlook.com>
2018-06-08 21:44:05 +0800

    Modified the way of export in Utils/Reg.js

--- utils/reg.js
+++ utils/reg.js
@@ -2,6 +2,8 @@ const phoneReg = /^1[34578]\d{9}$/;
 const userNameReg = /^[a-z0-9]+$/i;
 const pwdReg = /^.*(?=.{8,})(?=.*\d)(?=.*[A-Z])(?=.*[a-z]).*$/;
 
-exports.phoneReg = phoneReg;
-exports.pwdReg = pwdReg;
-exports.userNameReg = userNameReg;
+exports.default = {
+    phoneReg,
+    pwdReg,
+    userNameReg,
+}
