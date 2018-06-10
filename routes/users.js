0482ed492e1e40346d476937ea3a6a393831e8f0
华鹏 <hexris@outlook.com>
2018-06-10 15:00:07 +0800

    Mudular the user.js

--- routes/users.js
+++ routes/users.js
@@ -1,88 +1,34 @@
 var express = require("express");
 var router = express.Router();
 
-// User Model
-var User = require("../models/user"); // 引入模型
+// 引入模型
+var User = require("../models/user"); // User Model
+
 // Reg Rule
 var { pwdReg, phoneReg, userNameReg } = require("../utils/reg");
 
-/* GET users listing. */
-router.get("/", function(req, res, next) {
-  res.send("respond with a resource");
-});
-
-/* GET users listing. */
 /**
  * Create User
  * @constructor
- * @param {Object} req - The Information of the user.
+ * @param {Object} req - Including The Information of the user.
  */
 
-router.post("/create", (req, res, next) => {
+router.post("/create", async (req, res, next) => {
   console.log("Create User");
   const { username, password, phone, openID, gender } = req.body;
 
-  valiData(username, password, phone, openID, gender);
-
   // 验证数据
-  async function valiData(username, password, phone, openID, gender) {
-    // 判断数据是否为空
-    if (username.length < 6 || !userNameReg.test(username)) {
-      res.send({ code: 400, msg: "用户名不合法" });
-      return;
-    } else if (!password || !pwdReg.test(password)) {
-      res.send({ code: 400, msg: "密码不合法" });
-      return;
-    } else if (!phone || !phoneReg.test(phone)) {
-      res.send({ code: 400, msg: "手机号不合法" });
-      return;
-    } else if (!openID) {
-      res.send({ code: 400, msg: "openID不合法" });
-      return;
-    } else if (!gender) {
-      res.send({ code: 400, msg: "性别不合法" });
-      return;
-    }
-    
-    // 判断数据是否合法
-    if (await checkUserName(username.toLowerCase())) {
-      res.send({ code: 303, msg: "用户名已存在" });
-      return;
-    } else if (await checkPhone(phone)) {
-      res.send({ code: 303, msg: "已绑定其他账号" });
-      return;
-    }
-
-    //保存
-    saveUserInfo();
-  }
-
-  // 用户名查重
-  function checkUserName(username) {
-    return User.findOne({ username });
-  }
-
-  // 手机号查重
-  function checkPhone(phone) {
-    return User.findOne({ phone });
-  }
-
-  // 保存用户信息
-  function saveUserInfo() {
-    var user = new User({
-      username: username.toLowerCase(),
-      password,
-      phone,
-      openID,
-      gender,
-      createTime: new Date().getTime()
-    });
-    user.save(err => {
-      //添加
-      console.log("save status:", err ? "failed" : "success");
-      (err && res.send(err)) || res.send({ code: 200, msg: "创建成功" });
-    });
-  }
+  var user = await valiData(
+    res,
+    username,
+    password,
+    phone,
+    openID,
+    gender
+  ).catch(error => {
+    console.log("valiData Error:", error);
+  });
+  user && saveUserInfo(user, res);
 });
 
 router.post("/login", function(req, res, next) {
@@ -103,4 +49,105 @@ router.post("/login", function(req, res, next) {
     }
   });
 });
+
+// Common Methods
+async function valiData(res, username, password, phone, openID, gender) {
+  // 判断数据是否为空
+  if (username.length < 6 || !userNameReg.test(username)) {
+    res.send({ code: 400, msg: "用户名不合法" });
+    return;
+  } else if (!password || !pwdReg.test(password)) {
+    res.send({ code: 400, msg: "密码不合法" });
+    return;
+  } else if (!phone || !phoneReg.test(phone)) {
+    res.send({ code: 400, msg: "手机号不合法" });
+    return;
+  } else if (!openID) {
+    res.send({ code: 400, msg: "openID不合法" });
+    return;
+  } else if (!gender) {
+    res.send({ code: 400, msg: "性别不合法" });
+    return;
+  }
+
+  // 判断用户名是否存在
+  let illegalName = await checkUserName(
+    username.toLowerCase(),
+    username
+  ).catch(error => {
+    res.send({ code: 400, data: `验证用户名错误，${error}` });
+    return;
+  });
+  
+  // 用户名已存在抛出错误
+  if (illegalName.result) {
+    res.send({ code: 400, msg: `用户名：${illegalName.rawName}，已存在！` });
+    return;
+  }
+
+  // 判断手机号是否被使用
+  let illegalPhone = await checkPhone(phone).catch(error => {
+    res.send({ code: 400, data: `验证手机号错误，${error}` });
+    return;
+  });
+
+  if (illegalPhone) {
+    res.send({
+      code: 400,
+      msg: `手机号：${illegalPhone.phone}，已绑定其他账号`
+    });
+    return;
+  }
+
+  // 实例化User，记录创建时间的
+  var user = new User({
+    username: username.toLowerCase(),
+    password,
+    phone,
+    openID,
+    gender,
+    createTime: new Date().getTime()
+  });
+
+  // //验证通过返回User实例
+  return user
+}
+
+// 用户名查重
+function checkUserName(username, rawName) {
+  return new Promise((resolve, reject) => {
+    User.findOne({ username }, (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve({ result, rawName });
+      }
+    });
+  });
+  // return User.findOne({ username });
+}
+
+// 手机号查重
+function checkPhone(phone) {
+  return new Promise((resolve, reject) => {
+    User.findOne({ phone }, (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+// 保存用户信息
+function saveUserInfo(user, res) {
+  user.save(err => {
+    //添加
+    console.log("save status:", err ? "failed" : "success");
+    err && res.send(err);
+    res && res.send({ code: 200, msg: "创建成功" });
+  });
+}
+
 module.exports = router;
--- utils/reg.js
+++ utils/reg.js
@@ -2,7 +2,7 @@ const phoneReg = /^1[34578]\d{9}$/;
 const userNameReg = /^[a-z0-9]+$/i;
 const pwdReg = /^.*(?=.{8,})(?=.*\d)(?=.*[A-Z])(?=.*[a-z]).*$/;
 
-exports.default = {
+module.exports = {
     phoneReg,
     pwdReg,
     userNameReg,
